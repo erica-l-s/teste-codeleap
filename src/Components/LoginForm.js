dfe506efedfaf049d840776a7936ce0d256b9526
erica-l-s <erica.l.s1996@gmail.com>
2023-04-13 18:16:57 -0300

    fazendo o login e estilizando

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const LoginForm = (user) => {
+
+    const navigate = useNavigate();
+    const [login, setLogin] = useState("");
+    const [isLogged, setIsLogged] = useState(false)
+
+    const onInput = (e) => setLogin(e.target.value);
+
+    const hadleSubmit = (e) => {
+        e.preventDefault()
+       navigate('/crud')
+        
+        const userData = {
+            login
+          };
+          localStorage.setItem("info", JSON.stringify(userData))
+          setIsLogged(true)
+          
+    
+    }
+    
+    const handleLogout = () =>{
+        localStorage.removeItem("info")
+        setIsLogged(false)
+        
+    }
+ 
+    return (
+            
+       <div className="container">
+        <form onSubmit={hadleSubmit}>
+            <h3>Welcome to CodeLeap network!</h3>
+
+            <p>Please enter your username</p>
+            <input
+                value={login}
+                onInput={onInput}
+                onChange={(e) => setLogin(e.target.value)}
+            />
+            <button
+                disabled={!login}
+            >Enter</button>
+
+
+        </form>
+      
+       </div>
+            
+   
+    )
+    
+}
+
+export default LoginForm
---
+++ src/Components/PageCrud.js
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
+
+
+const PageCrud = (user) =>{
+  
+
+    return(
+     <div>
+        <h2>Crud</h2>
+       <button></button>
+     </div>
+    )
+}
+
+export default PageCrud
