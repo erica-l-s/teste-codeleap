5b2d5d3506cda61e794965d09cc19813e881bba2
erica-l-s <erica.l.s1996@gmail.com>
2023-04-13 21:33:49 -0300

    login e logout okay

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,35 +1,23 @@
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import React from "react";
+import PageCrud from "./PageCrud";
 
 const LoginForm = (user) => {
 
-    const navigate = useNavigate();
     const [login, setLogin] = useState("");
     const [isLogged, setIsLogged] = useState(false)
 
     const onInput = (e) => setLogin(e.target.value);
 
-    const hadleSubmit = (e) => {
-        e.preventDefault()
-       navigate('/crud')
-        
-        const userData = {
-            login
-          };
-          localStorage.setItem("info", JSON.stringify(userData))
+    const hadleSubmit = () => {
+   
+          localStorage.setItem("info", JSON.stringify(login))
           setIsLogged(true)
-          
-    
     }
     
-    const handleLogout = () =>{
-        localStorage.removeItem("info")
-        setIsLogged(false)
-        
-    }
- 
+  
     return (
-            
+          ( isLogged ? <PageCrud/> : 
        <div className="container">
         <form onSubmit={hadleSubmit}>
             <h3>Welcome to CodeLeap network!</h3>
@@ -48,8 +36,8 @@ const LoginForm = (user) => {
         </form>
       
        </div>
-            
-   
+              
+        ) 
     )
     
 }
--- src/Components/PageCrud.js
+++ src/Components/PageCrud.js
@@ -1,21 +1,44 @@
+import { useState } from "react";
+import React from "react";
+
+
 const PageCrud = () =>{
+    const [title, setTitle] = useState("")
+   
+    const onInput = (e) => setTitle(e.target.value);
+    const handlerClick = () =>{
+        localStorage.clear()
+        window.location.reload()
+
+    }
+    
+   
    return(
     <div className="container-crud">
            <div className="header">
                <p>CodeLeap Network</p>
+               <button onClick={handlerClick}>Logout</button>
            </div>
            <div className="box">
             <h2>What's on your mind?</h2>
              
              <div>
              <p>Title</p>
-             <input className="input-title"></input>
+             <input 
+             className="input-title"
+             value={title}
+             onInput={onInput}
+             ></input>
              </div>
              <div>
               <p>Content</p>
-              <input className="input-content"></input>
+              <input 
+              className="input-content"
+             ></input>
              </div>
-              <button>Create</button>
+              <button
+              disabled={!title}
+              >Create</button>
            </div>
 
        </div>
